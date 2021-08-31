a409a348104806a80b5c3be40a637604b8d93a8f
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-08-31 13:50:02 +0200

    Update fix-config.sh

--- install/fix-config.sh
+++ install/fix-config.sh
@@ -5,14 +5,16 @@
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
 file=install-config.yaml						;
+# MINIMUM master_type: t3a.xlarge
+# MINIMUM worker_type: t3a.large
 #########################################################################
 sed --in-place 								\
 	/' 'platform/d 							\
-	$file								;
+	${file}								;
 sed --in-place 								\
-	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: t3a.xlarge'/ \
-	$file								;
+	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: '${master_type}/ \
+	${file}								;
 sed --in-place 								\
-	/'name.*worker/s/^.*$/  name: worker\n  platform:\n    aws:\n      type: t3a.large'/ \
-	$file								;
+	/'name.*worker/s/^.*$/  name: worker\n  platform:\n    aws:\n      type: '${worker_type}/ \
+	${file}								;
 #########################################################################
