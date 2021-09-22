4557dd071414ac4d9fed976d392829fded89eedf
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-09-22 05:22:58 +0200

    Update ocp-aws-install.sh

--- install/ocp-aws-install.sh
+++ install/ocp-aws-install.sh
@@ -1,10 +1,3 @@
-# git clone https://github.com/academiaonline/openshift
-# cd openshift/
-# git checkout master
-# cd install/
-# vi 00-env.sh
-# source ocp-aws-install.sh
-
 pwd=${PWD}
 source 00-env.sh
 source 01-initial.sh
@@ -12,4 +5,3 @@ source 01-initial.sh
 cd ${pwd}
 source 02-create.sh
 source 03-certs.sh
-
