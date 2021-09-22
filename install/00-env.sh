fb59f7344095bc9d70b97636f142e7b9887315cd
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-09-22 04:37:11 +0200

    Update 00-env.sh

--- install/00-env.sh
+++ install/00-env.sh
@@ -2,6 +2,6 @@ export ClusterName=openshift
 export DomainName=sebastian-colomar.es 
 export master_type=t3a.xlarge
 export Publish=External
-export version=4.8.10
+export version=4.8.11
 export worker_type=t3a.large
 
