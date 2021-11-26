7e00468ad2169015ce6cd292f4ceec2fd1ead058
Sebastian Colomar <sebastian.colomar@gmail.com>
2021-11-26 07:35:56 +0100

    Update 02-create.sh

--- install/02-create.sh
+++ install/02-create.sh
@@ -1,4 +1,4 @@
-cp ${dir}/install-config.yaml ${dir}/install-config.yaml.$( date +%F_%H%M )
+cp ${dir}/install-config.yaml ${dir}/install-config.yaml.bak
 
 openshift-install-${version} create cluster --dir ${dir} --log-level debug
 
