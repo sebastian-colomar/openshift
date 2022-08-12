429507c1580ad12848601bcc9a976172c86a711b
Sebastian Colomar <sebastian.colomar@gmail.com>
2022-08-13 00:35:48 +0200

    Update fix-config.sh

--- install/fix-config.sh
+++ install/fix-config.sh
@@ -7,14 +7,36 @@
 file=install-config.yaml						;
 # MINIMUM master_type: t3a.xlarge
 # MINIMUM worker_type: t3a.large
+# ClusterNetworkCIDR=10.128.0.0/16
+# MachineNetworkCIDR=10.0.0.0/16
+# Region=ap-south-1
+# ServiceNetworkCIDR=172.30.0.0/16
 #########################################################################
 sed --in-place 								\
 	/' 'platform/d 							\
 	${file}								;
 sed --in-place 								\
-	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: '${master_type}/ \
+	/'cidr: 10.128.0.0.14'/s/10.128.0.0/$( echo ${ClusterNetworkCIDR} | cut -d/ -f1 )/		\
 	${file}								;
 sed --in-place 								\
-	/'name.*worker/s/^.*$/  name: worker\n  platform:\n    aws:\n      type: '${worker_type}/ \
+	/'cidr: 10.128.0.0.14'/s/14/$( echo ${ClusterNetworkCIDR} | cut -d/ -f2 )/			\
+	${file}								;
+sed --in-place 								\
+	/'cidr: 10.0.0.0.16'/s/10.0.0.0/$( echo ${MachineNetworkCIDR} | cut -d/ -f1 )/			\
+	${file}								;
+sed --in-place 								\
+	/'cidr: 10.0.0.0.16'/s/16/$( echo ${MachineNetworkCIDR} | cut -d/ -f2 )/			\
+	${file}								;
+sed --in-place 								\
+	/'- 172.30.0.0.16'/s/172.30.0.0/$( echo ${ServiceNetworkCIDR} | cut -d/ -f1 )/			\
+	${file}								;
+sed --in-place 								\
+	/'- 172.30.0.0.16'/s/16/$( echo ${ServiceNetworkCIDR} | cut -d/ -f2 )/				\
+	${file}								;
+sed --in-place 								\
+	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: '${master_type}/ 	\
+	${file}								;
+sed --in-place 								\
+	/'name.*worker/s/^.*$/  name: worker\n  platform:\n    aws:\n      type: '${worker_type}/ 	\
 	${file}								;
 #########################################################################
