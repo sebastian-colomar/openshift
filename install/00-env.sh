d4d8d6a43b3a047e43ef1b9759cf947c0d334d00
Sebastian Colomar <sebastian.colomar@gmail.com>
2022-08-13 00:40:09 +0200

    Update 00-env.sh

--- install/00-env.sh
+++ install/00-env.sh
@@ -1,6 +1,9 @@
 export ClusterName=openshift
-export DomainName=sebastian-colomar.es 
+export ClusterNetworkCIDR=10.128.0.0/16
+export DomainName=sebastian-colomar.es
+export MachineNetworkCIDR=10.0.0.0/16
 export master_type=t3a.xlarge
 export Publish=External
+export ServiceNetworkCIDR=172.30.0.0/16
 export version=4.10.24
 export worker_type=t3a.large
