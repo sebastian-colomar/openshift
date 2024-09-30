1ab5e9fbc0c44b638bbbfd3a7026534d5a94be6c
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-09-30 15:22:24 +0200

    Create bmc-secureboot-disable.sh

---
+++ baremetal/bmc-secureboot-disable.sh
@@ -0,0 +1,12 @@
+source ~/credentials.txt;
+
+path=1/SecureBoot;
+
+for type in master worker;do 
+  for x in 0 1 2;do 
+    host=${type}-${x}.bmc;
+    echo;
+    echo $host:;
+    curl -d '{"SecureBootEnable": false}' -H "Authorization: Basic $bmc_token" -H "Content-type: application/json" -k -s -X PATCH https://$host/redfish/v1/Systems/$path;
+  done;
+done
