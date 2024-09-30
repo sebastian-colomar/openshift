b07ffac6f140dabb235244757ca02c8f181b4d9f
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-09-30 15:26:15 +0200

    Create bmc-poweroff.sh

---
+++ baremetal/bmc-poweroff.sh
@@ -0,0 +1,12 @@
+source ~/credentials.txt
+
+command='chassis power off'
+
+for type in master worker;do 
+  for x in 0 1 2;do 
+    host=${type}-${x}.bmc;
+    echo;
+    echo $host:;
+    ipmitool -H $host -I lanplus -P $bmc_password -U $bmc_username $command;
+  done;
+done
