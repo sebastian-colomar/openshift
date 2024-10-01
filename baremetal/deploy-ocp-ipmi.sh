1e0301b91509098713d2528b388d8eae59d3b1da
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 13:18:28 +0200

    Update deploy-ocp-ipmi.sh

--- baremetal/deploy-ocp-ipmi.sh
+++ baremetal/deploy-ocp-ipmi.sh
@@ -1,13 +1,13 @@
 openshift-baremetal-install --dir ~/clusterconfigs --log-level debug destroy cluster
 
-source wipe.sh
-source bmc-secureboot-disable.sh
-source bmc-secureboot-status.sh|grep -E ".SecureBootCurrentBoot.:.Disabled.|.SecureBootEnable.:false" -B1
-source bmc-poweroff.sh
+source ./wipe.sh
+source ./bmc-secureboot-disable.sh
+source ./bmc-secureboot-status.sh|grep -E ".SecureBootCurrentBoot.:.Disabled.|.SecureBootEnable.:false" -B1
+source ./bmc-poweroff.sh
 
-source virsh-remove.sh
-source pull-release.sh
-source install-config_ipmi.sh
+source ./virsh-remove.sh
+source ./pull-release.sh
+source ./install-config_ipmi.sh
 
 podman rm -f rhcos_image_cache
 podman run -d --name rhcos_image_cache -p 8080:8080/tcp -v /home/kni/rhcos_image_cache:/var/www/html registry.access.redhat.com/ubi9/httpd-24
@@ -15,5 +15,5 @@ podman run -d --name rhcos_image_cache -p 8080:8080/tcp -v /home/kni/rhcos_image
 openshift-baremetal-install --dir ~/clusterconfigs --log-level debug create manifests
 openshift-baremetal-install --dir ~/clusterconfigs --log-level debug create cluster
 
-source wait-for-no-co.sh
-source secureboot-reenable.sh
+source ./wait-for-no-co.sh
+source ./secureboot-reenable.sh
