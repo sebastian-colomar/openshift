62af75435188abec5b667cd02ffd04159ec5bb8b
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 14:28:20 +0200

    Update wait-for-no-co.sh

--- baremetal/wait-for-no-co.sh
+++ baremetal/wait-for-no-co.sh
@@ -1,24 +1,28 @@
-source ~/credentials.txt
+source ~/credentials.txt;
 
-sleep 10
-
-while true;do
-oc get no|grep NotReady.*control-plane&&continue
-oc get no|grep 0.*Ready.*control-plane&&oc get no|grep 1.*Ready.*control-plane&&oc get no|grep 2.*Ready.*control-plane&&break
-oc get no;sleep 10
-done
+sleep 10;
 
 while true;do
-oc get co|awk '{print $3}'|grep True  -q&&oc get co|awk '{print $3}'|grep -vE "AVAILABLE|True"||break
-oc get co;sleep 10
-done
+  oc get no|grep NotReady.*control-plane&&continue;
+  oc get no|grep 0.*Ready.*control-plane&&oc get no|grep 1.*Ready.*control-plane&&oc get no|grep 2.*Ready.*control-plane&&break;
+  oc get no;
+  sleep 10;
+done;
 
 while true;do
-oc get co|awk '{print $4}'|grep False -q&&oc get co|awk '{print $4}'|grep -vE "PROGRESSING|False"||break
-oc get co;sleep 10
-done
+  oc get co|awk '{print $3}'|grep True  -q&&oc get co|awk '{print $3}'|grep -vE "AVAILABLE|True"||break;
+  oc get co;
+  sleep 10;
+done;
 
 while true;do
-oc get co|awk '{print $5}'|grep False -q&&oc get co|awk '{print $5}'|grep -vE "DEGRADED|False"||break
-oc get co;sleep 10
+  oc get co|awk '{print $4}'|grep False -q&&oc get co|awk '{print $4}'|grep -vE "PROGRESSING|False"||break;
+  oc get co;
+  sleep 10;
+done;
+
+while true;do
+  oc get co|awk '{print $5}'|grep False -q&&oc get co|awk '{print $5}'|grep -vE "DEGRADED|False"||break;
+  oc get co;
+  sleep 10;
 done
