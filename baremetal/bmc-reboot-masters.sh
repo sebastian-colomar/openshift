9788253ef14e5300a8bd5bdae5ea509ede3db740
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 14:03:37 +0200

    Create bmc-reboot-masters.sh

---
+++ baremetal/bmc-reboot-masters.sh
@@ -0,0 +1,12 @@
+source ~/credentials.txt;
+
+command='chassis power cycle';
+
+for type in master;do 
+  for x in 0 1 2;do 
+    host=${type}-${x}.bmc;
+    echo;
+    echo $host:;
+    ipmitool -H $host -I lanplus -P $bmc_password -U $bmc_username $command;
+  done;
+done;
