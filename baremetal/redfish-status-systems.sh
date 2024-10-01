62174b9221d34d941b0210ecab436d52bbd5c99d
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 13:53:27 +0200

    Create redfish-status-systems.sh

---
+++ baremetal/redfish-status-systems.sh
@@ -0,0 +1,14 @@
+source ~/credentials.txt
+
+type=master;
+x=0;
+
+host=${type}-${x};
+PASS=$bmc_password;
+USER=$bmc_username;
+
+Server=$host.bmc;
+Target=Systems;
+TargetID=1;
+
+curl -H "Content-Type: application/json" -k -L -s -u $USER:$PASS -X GET https://$Server/redfish/v1/$Target/$TargetID/|jq .|tee redfish-status-$Target-$host.json;
