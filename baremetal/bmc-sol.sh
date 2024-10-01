2688a7d1958ef98988de620d0bed5b0c3b8b1fdf
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 13:24:43 +0200

    Create bmc-sol.sh

---
+++ baremetal/bmc-sol.sh
@@ -0,0 +1,10 @@
+source ~/credentials.txt;
+
+command='sol activate';
+
+type=master;
+x=0;
+host=${type}-${x}.bmc;
+echo;
+echo $host:;
+ipmitool -H $host -I lanplus -P $bmc_password -U $bmc_username $command;
