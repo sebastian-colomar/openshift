bcfe096a57046b694c15dffaf5e4ad57821d0bc2
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 13:32:22 +0200

    Create redfish-mount-virtual.sh

---
+++ baremetal/redfish-mount-virtual.sh
@@ -0,0 +1,14 @@
+source ~/credentials.txt
+
+type=master;
+x=0;
+
+host=${type}-${x}.bmc;
+PASS=$bmc_password;
+USER=$bmc_username;
+
+Server=$host;
+Target=Managers;
+TargetID=1/VirtualMedia/EXT1;
+
+curl -d '{"Image": "https://example.com/test.iso", "TransferProtocolType": "HTTPS", "UserName": "", "Password":"", "Inserted": true}' -H "If-Match: *" -H "Content-Type: application/json" -k -L -s -u $USER:$PASS -X PATCH https://$Server/redfish/v1/$Target/$TargetID
