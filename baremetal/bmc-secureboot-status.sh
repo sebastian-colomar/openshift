1e7964392595c32afda4325c6ab27da0c3faaa21
Sebastian Colomar <sebastian.colomar@gmail.com>
2024-10-01 14:16:01 +0200

    Update bmc-secureboot-status.sh

--- baremetal/bmc-secureboot-status.sh
+++ baremetal/bmc-secureboot-status.sh
@@ -1,6 +1,6 @@
-source ~/credentials.txt
+source ~/credentials.txt;
 
-path=1/SecureBoot
+path=1/SecureBoot;
 
 for type in master worker;do 
   for x in 0 1 2;do 
@@ -9,4 +9,4 @@ for type in master worker;do
     echo $host:;
     curl -d '{"SecureBootEnable": false}' -H "Authorization: Basic $bmc_token" -H "Content-type: application/json" -k -s -X GET https://$host/redfish/v1/Systems/$path;
   done;
-done
+done;
