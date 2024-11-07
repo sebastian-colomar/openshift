cd5df54fcada372bad217b495572540db6efc103
academiaonline <73463321+academiaonline@users.noreply.github.com>
2024-11-07 03:00:24 +0100

    Update 01-initial.sh

--- master/install/01-initial.sh
+++ master/install/01-initial.sh
@@ -11,16 +11,16 @@ git config --global user.email you@example.com
 git add .
 git commit -m Initial 
 
-if ! test -f ${HOME}/bin/openshift-install-${version}
+if ! test -f /usr/local/bin/openshift-install-${version}
 then
 
 modes='client install'
 for mode in ${modes}
   do
-    curl -O https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}/openshift-${mode}-linux-${version}.tar.gz
-    gunzip openshift-${mode}-linux-${version}.tar.gz
-    tar xf openshift-${mode}-linux-${version}.tar
-    rm -f openshift-${mode}-linux-${version}.tar
+    curl -O https://mirror.openshift.com/pub/openshift-v4/$(arch)/clients/ocp/${version}/openshift-${mode}-linux.tar.gz
+    gunzip openshift-${mode}-linux.tar.gz
+    tar xf openshift-${mode}-linux.tar
+    rm -f openshift-${mode}-linux.tar
   done
 
 binaries='oc openshift-install'
