9a64dda284d43c8faa7cda558b0a0188b3f37320
Sebastian Colomar <sebastian.colomar@gmail.com>
2025-06-02 07:51:06 +0200

    Update 02-create.sh

--- master/install/02-create.sh
+++ master/install/02-create.sh
@@ -5,4 +5,6 @@ openshift-install-${version} create cluster --dir ${dir} --log-level debug
 
 export KUBECONFIG=${dir}/auth/kubeconfig
 
-
+oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
+oc apply -f oauth.yaml
+oc adm policy add-cluster-role-to-user cluster-admin root
