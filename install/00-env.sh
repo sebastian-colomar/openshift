363167a4299fa9bf7fdd7d4ca51928b0c3df49de
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-04 13:33:08 +0100

    Update 00-env.sh

--- install/00-env.sh
+++ install/00-env.sh
@@ -10,8 +10,8 @@ export worker_type=t3a.large
 # while read -r region ami; do printf "%-15s %-20s : " "$region" "$ami"; aws ec2 describe-images --region "$region" --image-ids "$ami" --query 'Images[0].State' --output text 2>/dev/null | grep -q available && echo "AVAILABLE" || echo "NOT FOUND"; done < regions_amis.txt
 # aws ec2 describe-images --region ap-southeast-1 --image-ids ami-0b574d30ee267107c
 # aws ec2 copy-image --source-region ap-southeast-1 --source-image-id ami-0b574d30ee267107c --region ap-south-1 --name "rhcos-48.84.202109241901-0"
-#{
-#    "ImageId": "ami-0e1b098ff482299d5"
-#}
+# {
+#     "ImageId": "ami-0e1b098ff482299d5"
+# }
 # aws ec2 describe-images --region ap-south-1 --image-ids ami-0e1b098ff482299d5
 export amiID=ami-0e1b098ff482299d5
