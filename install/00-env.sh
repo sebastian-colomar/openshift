197f2107f538faffa0e1cacf2a4272f78eac0b66
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-04 18:48:15 +0100

    Update 00-env.sh

--- install/00-env.sh
+++ install/00-env.sh
@@ -1,17 +1,18 @@
-export ClusterName=openshift
-export DomainName=sebastian-colomar.es 
-export master_type=t3a.xlarge
+export ClusterName=hub
+export DomainName=sebastian-colomar.com 
+export master_type=t3a.2xlarge
 export Publish=External
-export version=4.8.33
-export worker_type=t3a.large
+export version=4.8.37
+export worker_type=t3a.2xlarge
 
-# openshift-install-4.8.33 coreos print-stream-json | tee stream.json
+# openshift-install-4.8.37 coreos print-stream-json | tee stream.json
 # jq -r '.architectures.x86_64.images.aws.regions | to_entries[] | "\(.key) \(.value.image)"' stream.json | sort | tee regions_amis.txt
 # while read -r region ami; do printf "%-15s %-20s : " "$region" "$ami"; aws ec2 describe-images --region "$region" --image-ids "$ami" --query 'Images[0].State' --output text 2>/dev/null | grep -q available && echo "AVAILABLE" || echo "NOT FOUND"; done < regions_amis.txt
-# aws ec2 describe-images --region ap-southeast-1 --image-ids ami-0b574d30ee267107c
-# aws ec2 copy-image --source-region ap-southeast-1 --source-image-id ami-0b574d30ee267107c --region ap-south-1 --name "rhcos-48.84.202109241901-0"
+#     us-east-1       ami-06e6531aef9359b1e : AVAILABLE
+# aws ec2 describe-images --region us-east-1 --image-ids ami-06e6531aef9359b1e
+# aws ec2 copy-image --source-region us-east-1 --source-image-id ami-06e6531aef9359b1e --region ap-south-1 --name "rhcos-48.84.202109241901-0"
 # {
-#     "ImageId": "ami-0e1b098ff482299d5"
+#     "ImageId": "ami-0fab511d32cc88793"
 # }
-# aws ec2 describe-images --region ap-south-1 --image-ids ami-0e1b098ff482299d5
-export amiID=ami-0e1b098ff482299d5
+# aws ec2 describe-images --region ap-south-1 --image-ids ami-0fab511d32cc88793
+export amiID=ami-0fab511d32cc88793
