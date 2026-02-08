b951d4ac8dc573ab51784d6169d9632ced8f1c70
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-08 01:18:27 +0100

    Update 01-initial.sh

--- install/01-initial.sh
+++ install/01-initial.sh
@@ -1,13 +1,17 @@
-test -f ${HOME}/.ssh/id_rsa.pub || ssh-keygen -f ${HOME}/.ssh/id_rsa -P ''
+test -f ${HOME}/.ssh/id_rsa || ssh-keygen -f ${HOME}/.ssh/id_rsa -P ''
 eval "$( ssh-agent -s )"
 ssh-add ${HOME}/.ssh/id_rsa 
+
+unalias rm cp mv
+export dir="${HOME}/environment/${ClusterName}.${DomainName}"
+test -d ${dir} || mkdir -p ${dir} 
 
 export BINARY_PATH=${HOME}/bin
 grep -q ":${BINARY_PATH}:" ~/.bashrc || echo "export PATH=\"${BINARY_PATH}:\${PATH}\"" | tee -a ~/.bashrc
 source ~/.bashrc
 
 
-if ! test -f ${BINARY_PATH}/bin/openshift-install-${version}
+if ! test -f ${BINARY_PATH}/openshift-install-${version}
 then
 modes='client install'
 for mode in ${modes}
@@ -18,35 +22,30 @@ for mode in ${modes}
     rm openshift-${mode}-linux-${version}.tar
   done
 
-mkdir -p ${BINARY_PATH}/bin
+mkdir -p ${BINARY_PATH}
 
 binaries='kubectl oc'
 for binary in ${binaries}
   do
-    mv ${binary} ${BINARY_PATH}/bin
+    mv ${binary} ${BINARY_PATH}
   done
-mv openshift-install ${BINARY_PATH}/bin/openshift-install-${version}
+mv openshift-install ${BINARY_PATH}/openshift-install-${version}
 
 file=README.md 
 test -f ${file} && rm -f ${file}
 
-file=${BINARY_PATH}/bin/openshift-install
+file=${BINARY_PATH}/openshift-install
 test -f ${file} && rm -f ${file}
 
-ln -s ${BINARY_PATH}/bin/openshift-install-${version} ${BINARY_PATH}/bin/openshift-install
-rm ${BINARY_PATH}/bin/kubectl && ln -s ${BINARY_PATH}/bin/oc ${BINARY_PATH}/bin/kubectl    
+ln -s ${BINARY_PATH}/openshift-install-${version} ${BINARY_PATH}/openshift-install
+rm -f ${BINARY_PATH}/kubectl
+ln -s ${BINARY_PATH}/oc ${BINARY_PATH}/kubectl    
 fi
 
 export dir="${HOME}/environment/${ClusterName}.${DomainName}"
 test -d ${dir} || mkdir -p ${dir} 
 
 openshift-install-${version} create install-config --dir ${dir} --log-level debug
-
-cd ${dir} && git init
-git config --global user.name 'Your Name'
-git config --global user.email you@example.com
-git add .
-git commit -m Initial 
 
 cd ${dir}
 cp -v ${pwd}/fix-config.sh .
