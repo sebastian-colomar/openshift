f372cca5a42f91fe1b795c5209370ffb4b8dc84e
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-08 01:11:54 +0100

    Update fix-config.sh

--- install/fix-config.sh
+++ install/fix-config.sh
@@ -11,11 +11,30 @@ file=install-config.yaml						;
 sed --in-place 								\
 	/' 'platform/d 							\
 	${file}								;
-
+sed --in-place 								\
+	/'cidr: 10.128.0.0.14'/s/14/$( echo ${ClusterNetworkCIDR} | cut -d/ -f2 )/			\
+	${file}								;
+sed --in-place 								\
+	/'cidr: 10.128.0.0'/s/10.128.0.0/$( echo ${ClusterNetworkCIDR} | cut -d/ -f1 )/			\
+	${file}								;
+sed --in-place 								\
+	/'hostPrefix: 23'/s/23/$( echo ${hostPrefix} )/							\
+	${file}								;
+sed --in-place 								\
+	/'cidr: 10.0.0.0.16'/s/16/$( echo ${MachineNetworkCIDR} | cut -d/ -f2 )/			\
+	${file}								;
+sed --in-place 								\
+	/'cidr: 10.0.0.0'/s/10.0.0.0/$( echo ${MachineNetworkCIDR} | cut -d/ -f1 )/			\
+	${file}								;
+sed --in-place 								\
+	/'- 172.30.0.0.16'/s/16/$( echo ${ServiceNetworkCIDR} | cut -d/ -f2 )/				\
+	${file}								;
+sed --in-place 								\
+	/'- 172.30.0.0'/s/172.30.0.0/$( echo ${ServiceNetworkCIDR} | cut -d/ -f1 )/			\
+	${file}								;
 sed --in-place \
   	/'name.*master/s/^.*$/  name: master\n  platform:\n    aws:\n      type: '${master_type}'\n      amiID: '${amiID}/ \
 	${file}
-
 sed --in-place 								\
   	/'name.*worker/s/^.*$/  name: worker\n  platform:\n    aws:\n      type: '${worker_type}'\n      amiID: '${amiID}/ \
 	${file}								;
